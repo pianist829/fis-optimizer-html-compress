71007c5d4b7164401701112b05c11540fd099e34
yuanfang829@163.com <pianist829@gmail.com>
2013-07-11 21:49:45 +0800

    [change]添加对smarty配置的合并

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ var HtmlCompress = require('html-compress');
 
 
 module.exports = function(content, file, conf){
-	return HtmlCompress.compress(content,conf);
+    fis.util.map(fis.config.get('setting.smarty'), function(key, value){
+        if(!conf.hasOwnProperty(key)){
+            conf[key] = value;
+        }
+    });
+    return HtmlCompress.compress(content,conf);
 };
 
 module.exports.defaultOptions = {
-    'level' : 'strip',
-    'leftDelimiter' : '{%',
-    'rightDelimiter' : '%}'
+    'level' : 'strip'
 };
